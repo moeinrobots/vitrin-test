e5f1d826220aa3f86db98101516e8ada9c4115fa
mehdi <mahdiopera110@gmail.com>
2026-06-29 10:54:04 +0330

    feat(features/user): create new feature folder for user.

---
+++ src/features/user/types/user.types.ts
@@ -0,0 +1,12 @@
+export interface UserType {
+    id: number;
+    username: string;
+    mobile_number: number;
+    email: string;
+    avatar: string;
+    first_name: string;
+    last_name: string;
+    full_name: string;
+    gender: string;
+    is_profile_completed: boolean;
+}
