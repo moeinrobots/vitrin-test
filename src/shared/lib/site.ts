5e78e4023e9ebb26bb2c3bdfeb3d438375007153
mehdi <mahdiopera110@gmail.com>
2026-06-30 11:02:11 +0330

    fix(site): fix site config informations.

--- src/shared/lib/site.ts
+++ src/shared/lib/site.ts
@@ -1,11 +1,7 @@
 export const siteConfig = {
-    name: process.env.SITE_NAME ?? 'Vitrin CMS',
-    description: 'Create and manage React applications with Vitrin CMS.',
-    url:
-        process.env.NEXT_PUBLIC_SITE_URL ??
-        (process.env.VERCEL_URL
-            ? `https://${process.env.VERCEL_URL}`
-            : 'https://vitrin.com'),
+    name: process.env.SITE_NAME ?? 'ویترین',
+    description: 'به سایت ساز ویترین خوش آمدید',
+    url: process.env.NEXT_PUBLIC_SITE_URL ?? 'https://vitrin.com',
 };
 
 export function getAbsoluteUrl(path = '/') {
