b631bbc8139f1f69bbf359f65181a01b03bebad7
mehdi <mahdiopera110@gmail.com>
2026-07-02 11:11:35 +0330

    fix(GlobalLoader): remove skeleton section.

--- src/shared/components/shared/GlobalLoader.tsx
+++ src/shared/components/shared/GlobalLoader.tsx
@@ -11,11 +11,11 @@ export function GlobalLoader() {
                     <div className="absolute inset-0 animate-spin rounded-full border-4 border-transparent border-t-primary" />
                 </div>
 
-                <div className="flex w-full flex-col gap-3">
+                {/* <div className="flex w-full flex-col gap-3">
                     <div className="h-3 w-2/3 animate-pulse rounded-full bg-muted" />
                     <div className="h-3 w-full animate-pulse rounded-full bg-muted" />
                     <div className="h-3 w-5/6 animate-pulse rounded-full bg-muted" />
-                </div>
+                </div> */}
             </div>
         </main>
     );
